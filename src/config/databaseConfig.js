209e9b22c6f249f87b35f34d50a61163e77bd67b
Bright Peter <brightbassey2001@gmail.com>
2022-10-25 12:14:34 +0100

    setup local database and migrations

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -2,6 +2,29 @@ const pgp = require("pg-promise")();
 require("dotenv").config();
 const db_url = process.env.DATABASE_URL;
 
-const database = pgp(db_url);
+// const database = pgp(db_url);
 
-module.exports = { database };
+const cn = {
+  host: "localhost",
+  port: 5432,
+  database: "atadb",
+  user: "george",
+  password: "password",
+};
+
+const tableCreator = async () => {
+  try {
+    const dt = await database.query(
+      `CREATE TABLE IF NOT EXISTS 
+        prod(name VARCHAR, 
+        price INT , 
+        description VARCHAR )`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const database = pgp(cn);
+
+module.exports = { database, tableCreator };
---
+++ src/controllers/productController.js
@@ -0,0 +1,22 @@
+// const { createProduct, listProducts } = require("../services/product");
+
+// const productCreate = async (req, res) => {
+//   try {
+//     const product = await createProduct(req.body);
+//     product && res.status(201).json("product created");
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// const productList = async (req, res) => {
+//   try {
+//     const products = await listProducts();
+//     // console.log(products);
+//     products && res.status(200).json(products);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// module.exports = { productCreate, productList };
